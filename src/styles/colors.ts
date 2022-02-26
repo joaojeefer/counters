4bf8db5823987f09d68cfd0c37bf93e8cbb4b2cb
João Jeefer <joao.felix@dbserver.com.br>
2022-02-25 21:20:27 -0300

    feat - add arquivos de estilizacao global

---
+++ src/styles/colors.ts
@@ -0,0 +1,30 @@
+const colors = {
+  primary: {
+    dark: '#001c47',
+    default: '#0082c9',
+    light: '#ff9500',
+  },
+  secondary: {
+    dark: '#333333',
+    default: '##8e8e93',
+    light: '#d8d8d8',
+    lightest: '#ffffff',
+  },
+  success: {
+    dark: '#33820d',
+    default: '#3fa110',
+    light: '#d7e6c8',
+  },
+  warning: {
+    default: '#ffcd00',
+    light: '#ffeb98',
+    dark: '#765f00',
+  },
+  danger: {
+    default: '#e60000',
+    light: '#ffb4b4',
+    dark: '#ab4745',
+  },
+};
+
+export default colors;
