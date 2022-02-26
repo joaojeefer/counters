eb108229103ba1605389ff6a8bb7b5ad88211c9f
João Jeefer <joao.felix@dbserver.com.br>
2022-02-26 11:21:16 -0300

    feat - ajuste no layout das abas do navegador

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 import {Config, Counters} from '../../screens';
 import {RootRoutes, RootTabsParamsList} from '../types';
+import {colors, metrics} from '../../styles';
 
 const {Navigator, Screen} = createBottomTabNavigator<RootTabsParamsList>();
+
+const tabsScreenProps: BottomTabNavigationOptions = {
+  headerShown: false,
+  tabBarStyle: {
+    height: 80,
+    backgroundColor: colors.primary.dark,
+  },
+  tabBarItemStyle: {height: 50},
+  tabBarLabelStyle: {
+    fontSize: metrics.font_size.xxx_small,
+  },
+  tabBarActiveTintColor: colors.primary.light,
+  tabBarInactiveTintColor: colors.secondary.default,
+};
 
 const Root: React.FC = () => {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{headerShown: false}}>
+      <Navigator screenOptions={tabsScreenProps}>
         <Screen name={RootRoutes.COUNTERS} component={Counters} />
         <Screen name={RootRoutes.CONFIG} component={Config} />
       </Navigator>
