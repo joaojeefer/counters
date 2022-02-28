d4d9c66694db77a511501686588549ab68a0045d
João Jeefer <joao.felix@dbserver.com.br>
2022-02-28 15:25:43 -0300

    feat - implementada tela de configuracoes

--- src/screens/Config/Config.tsx
+++ src/screens/Config/Config.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
-import {View} from 'react-native';
-
-// import { Container } from './styles';
+import React, {useContext, useState} from 'react';
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {ActionButton, Header} from '../../components';
+import {CounterContext} from '../../contexts/CounterContext';
+import styles from './styles';
 
 const Config: React.FC = () => {
-  return <View />;
+  const [increment, setIncrement] = useState('0');
+  const [decrement, setDecrement] = useState('0');
+
+  const {
+    counters,
+    addCounter,
+    removeActiveCounter,
+    incrementActiveCounter,
+    decrementActiveCounter,
+    resetActiveCounter,
+  } = useContext(CounterContext);
+
+  return (
+    <View style={styles.container}>
+      <Header title="Config" />
+      <ScrollView style={styles.scrolledContent}>
+        <Text style={styles.titleText}>Counters</Text>
+        <View style={styles.buttonsArea}>
+          <ActionButton title="Add Counter" onPress={addCounter} />
+          <View style={styles.betweenButtons} />
+          <ActionButton title="Remove Counter" onPress={removeActiveCounter} />
+        </View>
+
+        <Text style={styles.titleText}>Selected Counter</Text>
+        <View style={styles.inputsArea}>
+          <View style={styles.inputRow}>
+            <TextInput
+              style={styles.input}
+              placeholder="0"
+              keyboardType="numeric"
+              onChangeText={setIncrement}
+              value={increment}
+              onSubmitEditing={() => incrementActiveCounter(Number(increment))}
+            />
+            <TouchableOpacity
+              disabled={counters.length < 1}
+              style={styles.inputButton}
+              onPress={() => incrementActiveCounter(Number(increment))}>
+              <Text style={styles.inputButtonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.inputRow}>
+            <TextInput
+              style={styles.input}
+              placeholder="0"
+              keyboardType="numeric"
+              onChangeText={setDecrement}
+              value={decrement}
+              onSubmitEditing={() => decrementActiveCounter(Number(decrement))}
+            />
+            <TouchableOpacity
+              disabled={counters.length < 1}
+              style={styles.inputButton}
+              onPress={() => decrementActiveCounter(Number(decrement))}>
+              <Text style={styles.inputButtonText}>-</Text>
+            </TouchableOpacity>
+          </View>
+          <ActionButton title="Reset Counter" onPress={resetActiveCounter} />
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default Config;
---
+++ src/screens/Config/styles.ts
@@ -0,0 +1,61 @@
+import {StyleSheet} from 'react-native';
+import {colors, metrics} from '../../styles';
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+  scrolledContent: {
+    flexGrow: 1,
+    backgroundColor: colors.primary.default,
+    padding: metrics.spacing.medium,
+  },
+  titleText: {
+    fontSize: metrics.font_size.medium,
+    fontWeight: 'bold',
+    color: colors.secondary.dark,
+  },
+  buttonsArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: metrics.spacing.small,
+  },
+  betweenButtons: {
+    marginHorizontal: metrics.spacing.small,
+  },
+  inputsArea: {
+    marginVertical: metrics.spacing.small,
+  },
+  inputRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: metrics.spacing.small,
+  },
+  input: {
+    height: metrics.spacing.x_large,
+    width: 100,
+    padding: 0,
+    paddingLeft: metrics.spacing.x_small,
+    backgroundColor: colors.secondary.light,
+    fontSize: metrics.font_size.medium,
+    color: colors.primary.dark,
+    borderColor: colors.primary.dark,
+    borderTopLeftRadius: metrics.radius.small,
+    borderBottomLeftRadius: metrics.radius.small,
+  },
+  inputButton: {
+    height: metrics.spacing.x_large,
+    width: metrics.spacing.xx_large,
+    paddingHorizontal: metrics.spacing.small,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.primary.dark,
+    borderTopRightRadius: metrics.radius.small,
+    borderBottomRightRadius: metrics.radius.small,
+  },
+  inputButtonText: {
+    fontSize: metrics.font_size.small,
+    color: colors.secondary.lightest,
+  },
+});
+
+export default styles;
